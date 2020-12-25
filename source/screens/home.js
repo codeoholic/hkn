bb8b1d150b9de9780d0da1699b58fbfab60b16b5
codeoholic <mohit.tyagi@protonmail.com>
2020-12-25 18:05:35 +0530

    feat: time ago for posts

--- source/screens/home.js
+++ source/screens/home.js
@@ -2,16 +2,24 @@ import React, { Component } from 'react';
 import { FlatList, SafeAreaView, Text, TouchableOpacity, View } from 'react-native';
 import { connect } from 'react-redux';
 import styles from '../styles';
+import RNBootSplash from "react-native-bootsplash";
+import moment from 'moment';
 
 class Home extends Component{
     constructor(props) {
         super(props);
+        setTimeout(function(){ RNBootSplash.hide({ duration: 250 }); }, 3000);
     }
     keyExtractor = (item, index) => index.toString();
     render_story = ({item, index}) => {
         return(
             <TouchableOpacity key={ index } style={ styles.story } onPress={ () => this.props.navigation.navigate("WebViewCustom", { url: item.url }) }>
-                <Text>{ item.title }</Text>
+                <Text style={ styles.font_16 }>{ item.title }</Text>
+                <View style={[ styles.margin_top_10, styles.split_view ]}>
+                    <Text style={ styles.font_12 }>{ item.score } points</Text>
+                    {/*CONVERTING UNIX TIMESTAMP WHICH IS IN SECONDS TO MILLISECONDS WHICH CAN BE PROCESSED BY JS*/}
+                    <Text style={ styles.font_12 }>{ moment( item.time * 1000 ).fromNow() }</Text>
+                </View>
             </TouchableOpacity>
         )
     }
--- source/styles/index.js
+++ source/styles/index.js
@@ -7,6 +7,19 @@ export default StyleSheet.create({
 		flex: 1,
 		backgroundColor: WHITE	
 	},
+	font_16:{
+		fontSize: 16
+	},
+	font_12:{
+		fontSize: 12
+	},
+	margin_top_10:{
+		marginTop: 10
+	},
+	split_view:{
+        flexDirection:'row',
+        justifyContent: 'space-between',
+    },
 	story:{
 		paddingLeft: 20,
 		paddingRight: 20,
